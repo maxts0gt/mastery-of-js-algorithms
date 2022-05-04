80617c9f4db16a7d1d7e09ccdcd96ddec39476b7
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-05-04 16:48:19 +0900

    solve finding max in sliding window

---
+++ find-max-in-sliding-window.js
@@ -0,0 +1,48 @@
+// time O(n) | space O(w)
+const findMaxInSlidingWindow = (array, w) => {
+	let result = [];
+	// checking if the array is empty
+	if (array.length === 0) {
+		return result;
+	}
+	// check if window size > array size
+	if (w > array.length) {
+		w = array.length;
+	}
+	// creating window size for solution
+	let window = [];
+
+	// find out max for the first window
+	for (let i = 0; i < w; i++) {
+		// for every element, remove previous smaller from window
+		while (
+			window.length > 0 &&
+			array[i] >= array[window[window.length - 1]]
+		) {
+			window.pop();
+		}
+		// add current element at the back of queue
+		window.push(i);
+	}
+	// appending the largest element to the result
+	result.push(array[window[0]]);
+	// remove all numbers that are smaller than current number from tail of list
+	for (let i = windowSize; i < nums.length; i++) {
+		while (
+			window.length > 0 &&
+			array[i] >= nums[window[window.length - 1]]
+		) {
+			window.pop();
+		}
+
+		// remove the first index from window deque
+		if (window.length > 0 && window[0] < i - windowSize) {
+			window.shift();
+		}
+		// add current element at the back of queue
+		window.push(i);
+		result.push(nums[window[0]]);
+	}
+	// and finally return result
+	return result;
+};
